0a2ed7d238bebab8aef1e2a87f93a1f622127a3c
Juber_Mulla <juberjilanimulla@gmail.com>
2025-08-29 18:02:24 +0530

    this is commit for video admin and user panel

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -5,6 +5,7 @@ import usercontactRouter from "./usercontactRouter.js";
 import usersubcribeRouter from "./usersubcribeRouter.js";
 import userreviewRouter from "./userreviewRouter.js";
 import usercommentRouter from "./usercommentRouter.js";
+import uservideoRouter from "./uservideoRouter.js";
 
 const userRouter = Router();
 
@@ -14,5 +15,6 @@ userRouter.use("/contact", usercontactRouter);
 userRouter.use("/subcribe", usersubcribeRouter);
 userRouter.use("/review", userreviewRouter);
 userRouter.use("/comment", usercommentRouter);
+userRouter.use("/video", uservideoRouter);
 
 export default userRouter;
---
+++ routes/user/uservideoRouter.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import videomodel from "../../model/videomodel.js";
+import {
+  successResponse,
+  errorResponse,
+} from "../../helpers/serverResponse.js";
+
+const uservideoRouter = Router();
+
+uservideoRouter.get("/getvideos", getvideosHandler);
+
+export default uservideoRouter;
+
+async function getvideosHandler(req, res) {
+  try {
+    const videos = await videomodel
+      .find({ published: true })
+      .sort({ createdAt: -1 });
+    if (!videos) {
+      return errorResponse(res, 404, "videos not uploaded");
+    }
+    return successResponse(res, "success", videos);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
