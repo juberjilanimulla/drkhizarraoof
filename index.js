4fd1ffa88208a50c82500ea91f6d5341f879a83f
Juber_Mulla <juberjilanimulla@gmail.com>
2025-11-13 11:49:25 +0530

    change index.js file

--- index.js
+++ index.js
@@ -23,11 +23,9 @@ app.use(
 );
 //middleware
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: "10mb" }));
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ extended: true, limit: "100mb" }));
-app.options("*", cors());
 
 //routes
 app.use("/api/auth", authRouter);
