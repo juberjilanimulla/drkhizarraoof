d87bcc834285bb2237efc44ed4c4112f0c090bbc
Juber_Mulla <juberjilanimulla@gmail.com>
2025-11-13 12:44:19 +0530

    index.js

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ app.use(
   )
 );
 
-// Body parser
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // ⭐ Correct CORS config
